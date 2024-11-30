7a448752de329b63854a7d81b6ecc984c6efb462
pisces2336 <pisces2336@gmail.com>
2024-11-30 15:35:20 +0900

    fix: バリデーション強化

--- src/modules/user/dto/create-user.input.ts
+++ src/modules/user/dto/create-user.input.ts
@@ -1,13 +1,15 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { MaxLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, MaxLength } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
   @Field()
+  @IsEmail()
   @MaxLength(255)
   email: string;
 
   @Field()
+  @IsNotEmpty()
   @MaxLength(255)
   password: string;
 }
